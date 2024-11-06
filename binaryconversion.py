721b0292afd8d6a2bee1491c3cc81e60f88b2628
First-TimeCoder ! <xuminxia.no2@gmail.com>
2024-11-06 19:34:50 +0000

    decimal to binary

---
+++ binaryconversion.py
@@ -0,0 +1,15 @@
+def decimalTobinary ( num):
+    if 
+if 0 <= num <= 15
+     
+
+def decimalTobinaryAny(num):
+ binary = ["0", "0", "0", "0", "0"
+  if num > 128 :
+    binary[0] = "1"
+    num = num-128
+if num => 0
+    print ("This cannot be converted into Binary")
+
+
+decimalTobinaryAny(100)
